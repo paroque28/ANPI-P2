020867af50029e4a6fdb77f4969cd2e1abf7d461
Francisco Alvarado Ferllini <ferllini_13@hotmail.com>
2017-03-29 17:07:22 -0600

    laguerre probado, division, polinomios de prueba

--- main.cpp
+++ main.cpp
@@ -2,21 +2,43 @@
 
 #include "src/laguerre.h"
 
+using namespace std;
+
 int main()
 {
 
     vector<complex<double>> p;
-// x^3 - 8x^2 - 13x + 140 = (x+4)(x-5)(x-7)
-    p.push_back(-1);
-    p.push_back(-1);
-    p.push_back(-1);
-    //p.push_back(1);
+    vector<complex<double>> p2;
+    vector<complex<double>> p3;
 
-    vector<complex<double>> roots = laguerre(p);
 
-    for(size_t i = 0; i < roots.size(); i++) {
-        cout << setprecision(10) << roots[i] << endl;
-    }
+
+    p.push_back(140);
+    p.push_back(-13);
+    p.push_back(-8);
+    p.push_back(1);
+
+    p2.push_back(1);
+    p2.push_back(1);
+    p2.push_back(1);
+    p2.push_back(1);
+
+    p3.push_back(-8);
+    p3.push_back(-4);
+    p3.push_back(2);
+    p3.push_back(-1);
+    p3.push_back(1);
+
+
+
+    complex<double > root = laguerre(p,complex<double>(0.0));
+    complex<double > root2 = laguerre(p2,complex<double>(0.0));
+    complex<double > root3 = laguerre(p3,complex<double>(0.0));
+
+
+    cout << setprecision(10)<<"p1 " << root <<endl;
+    cout << setprecision(10)<<"p2 " << root2 <<endl;
+    cout << setprecision(10)<<"p3 " << root3 <<endl;
 
     return 0;
 }
